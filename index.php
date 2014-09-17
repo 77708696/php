79bb1ea392df475b277b8f3a7c14f6d7c97774d1
xiaoming.huang <huangxm@oradt.com>
2014-09-17 14:28:13 +0800

    modfiy

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
+echo "<br><br><br><br><br>";
+phpinfo();
+exit();
 echo crc32("abc") . "\r\n";
 
 $binary = "11111001";
 $hex = dechex(bindec($binary));
 echo $hex;
+echo print_r($_SERVER);
